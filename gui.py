5c989cd9559c69a0c3c5f7cbc6aa3bc6f222fbbb
hreherch <hreherch@ualberta.ca>
2016-02-28 18:57:00 -0700

    You can now log in to an oracle account using the login button. Error checking handled. If the main window is closed appropriately, will close the open connection if it exists.

--- gui.py
+++ gui.py
@@ -1,5 +1,64 @@
-
 from tkinter import *
+import tkinter.messagebox as tm
+import cx_Oracle
+
+global_userCx = None
+
+def login():
+    global global_userCx
+    
+    if global_userCx != None:
+        tm.showerror( "Login Error", "You are already connected!" )
+        return
+        
+    
+    top = Toplevel()
+    top.title( "Oracle Login" )
+    
+    #src: http://stackoverflow.com/questions/28156719/how-can-i-integrate-tkinter-with-python-log-in-screen
+    # I applied it without an object however...
+    
+    label1 = Label( top, text="Username" )
+    label2 = Label( top, text="Password" )
+    
+    entry1 = Entry( top )
+    entry2 = Entry( top, show="*" )
+    
+    label1.grid( row=0, sticky=E )
+    label2.grid( row=1, sticky=E )
+    entry1.grid( row=0, column=1 )
+    entry2.grid( row=1, column=1 )
+    
+    def performLogin():
+        try:
+            username = entry1.get()
+            password = entry2.get()
+            
+            con_string = username + "/" + password + "@gwynne.cs.ualberta.ca:1521/CRS"
+            
+            global global_userCx
+            global_userCx = cx_Oracle.connect( con_string )
+            
+            successInfo = "You have successfully connected to: " + global_userCx.dsn
+            tm.showinfo( "Login Success", successInfo )  
+            
+            top.destroy()
+            
+        except:
+            tm.showerror( "Login Error", "Check your username and password! \ner_nox02" )
+    
+    loginButton = Button( top, text="login", command=performLogin )
+    loginButton.grid( columnspan=2 )
+
+def disconnect():
+    try:
+        print( "attempting to close:", global_userCx.dsn )
+        global_userCx.close()
+        print( "close successful." )
+    except:
+        print( "WARNING: Connection may not have terminated properly." )
+
+    exit()
 
 # NEW VEHICLE REGISTRATION
 def app1_command():
@@ -95,7 +154,9 @@ def app5_command():
 top = Tk()
 top.wm_title( "ARS v0228" )
 top.resizable( width=FALSE, height=FALSE )
-top.geometry( '{}x{}'.format( 250, 300 ) )
+top.geometry( '{}x{}'.format( 250, 365) )
+
+top.protocol( 'WM_DELETE_WINDOW', disconnect )
 
 # App1 =========================================================================
 info1 = "New Vehicle Registration:"
@@ -146,5 +207,15 @@ top.app5_button = Button( top, padx=5, pady=5, width=50 )
 top.app5_button["text"] = "goto"
 top.app5_button["command"] = app5_command
 top.app5_button.pack( side="top" )
+
+# Login ===========================================================
+info6 = ""
+msg6 = Message( top, text=info6, padx=5, pady=5, width=200 )
+msg6.pack()
+
+top.app6_button = Button( top, padx=5, pady=5, width=50 )
+top.app6_button["text"] = "login"
+top.app6_button["command"] = login
+top.app6_button.pack( side="top" )
 
 top.mainloop()
