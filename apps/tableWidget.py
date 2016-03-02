48273730ae595b53c76eaf810ff17de71527a7b3
hreherch <hreherch@ualberta.ca>
2016-03-02 11:53:00 -0700

    Prototype for tableWidget is working. Need to work on functionality for app5.

--- apps/tableWidget.py
+++ apps/tableWidget.py
@@ -3,31 +3,32 @@
 from tkinter import *
 import tkinter.messagebox as tm
 
-class tableWidget():
-    def __init__( self, rows=3, columns=3 ):
-        
+class tableWidget( Frame ):
+    def __init__( self, numRows = 4, numCols = 4 ):
+        self.rows = numRows + 1
+        self.cols = numCols
         self.root = Tk()
-        self.rows = rows
-        self.columns = columns
+        self.root.configure( background="black" )
+        
         self.table = []
         
-        for row in range( rows ):
-            for col in range( columns ):
-                print( row, col )
-                self.table.append( Label( self.root, text="hi" ) )
-                
-                
-        for row in self.table:
-            print( row )
-                
-                
-    def addEntry( self, row, col, entry ):
-        print( self.table )
-        self.table[row][col] = entry
+        for row in range( self.rows ):
+            for col in range( self.cols ):
+                thisLabel = Label( self.root, text="" )
+                thisLabel.grid( row=row, column=col, padx=1, pady=1, sticky=EW )
+                self.table.append( thisLabel )
         
-    def printTable( self ):
-        print( self.table )
+    def changeColHeader( self, col, Header ):
+        self.table[ col ].configure( text=Header )
+    
+    def changeEntry( self, row, col, Text ):
+        row = row + 1
+        self.table[ (row * self.cols) + col ].configure( text=Text )
         
-a = tableWidget( rows = 4, columns = 5)
-#a.addEntry( 1, 1, "poop" )
-#a.printTable()
+    def showTable( self ):
+        mainloop()
+        
+test = tableWidget()
+test.changeColHeader( 1, "Random Things" )
+test.changeEntry( 1, 1, "double" )
+test.showTable()
