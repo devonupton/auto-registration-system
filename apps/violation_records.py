a434eb8f21b54d7c6ad33e4cd02ae8ce3da006d9
hreherch <hreherch@ualberta.ca>
2016-03-08 12:11:04 -0700

    Started app4.

--- apps/violation_records.py
+++ apps/violation_records.py
@@ -1,9 +1,40 @@
 ''' Violation Records Application '''
 
+#Violation Record: 
+    # This component is used by a police officer to issue a traffic ticket and record 
+    # the violation. You may assume that all the information about ticket_type has been 
+    # loaded in the initial database.
+
+#CREATE TABLE ticket (
+#  ticket_no     int,
+#  violator_no   CHAR(15),  
+#  vehicle_id    CHAR(15),
+#  office_no     CHAR(15),
+#  vtype        char(10),
+#  vdate        date,
+#  place        varchar(20),
+#  descriptions varchar(1024),
+#  PRIMARY KEY (ticket_no),
+#  FOREIGN KEY (vtype) REFERENCES ticket_type,
+#  FOREIGN KEY (violator_no) REFERENCES people ON DELETE CASCADE,
+#  FOREIGN KEY (vehicle_id)  REFERENCES vehicle,
+#  FOREIGN KEY (office_no) REFERENCES people ON DELETE CASCADE
+    
 from tkinter import *
 import tkinter.messagebox as tm
 import cx_Oracle
 
 def run(connection):
-    print("Access: App4")
-    return
+    # check login soon
+    # poop
+    pass
+    
+class app4( Toplevel ):
+    def __init__( self, userCx ):
+        TopLevel.__init__( self ) # might need to consider parent?
+        self.title( "App5: Violation Insertion" )
+        
+        self.userCx = userCx
+        
+        # build window
+        return
